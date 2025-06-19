7e55f26aa8df841b60e170083d8596d6830912b9
JONAHBRUZZI <jon.guerra@duocuc.cl>
2025-06-18 22:20:12 -0400

    v3

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,5 +0,0 @@
-import { c, s } from "./chunks/entrypoint_xQvDO3fV.mjs";
-export {
-  c as createExports,
-  s as start
-};
--- .vercel/output/static/_astro/astro_scripts/before-hydration.js.DFsOlWCZ.js
+++
@@ -1,1 +0,0 @@
-import{r}from"../index.BDIHttqv.js";import"../chunk-K6S4O6NY.RSQ76uOP.js";await r({afterSignInUrl:"/",afterSignUpUrl:"/",sdkMetadata:{version:"2.9.1",name:"@clerk/astro",environment:"production"}});
